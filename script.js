1c8b8e02b5666db66b2e1661342cf12f62d587f3
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-03-02 14:18:44 -0300

    2 files modified

--- script.js
+++ script.js
@@ -1,52 +1,119 @@
 const quizs = [
   {
+    id:0,
+    question:"esta es una pregunta para la quiz?",
+    options:{
+      option1:{
+        answer:"esta es la respuesta 1",
+        checked:""
+      },
+      option2:{
+        answer:"esta es la respuesta 2",
+        checked:""
+      },
+      option3:{
+        answer:"esta es la respuesta 3",
+        checked:""
+      },
+     
+    },
+    answerCorrect:"esta es la respuesta 1",
+    answer:"",
+    score:0
+  },
+  {
     id:1,
     question:"esta es una pregunta para la quiz?",
     options:{
-      option1:"esta es la respuesta 1?",
-      option2:"esta es la respuesta 2?",
-      option3:"esta es la respuesta 3?"
+      option1:{
+        answer:"esta es la respuesta 1",
+        checked:""
+      },
+      option2:{
+        answer:"esta es la respuesta 2",
+        checked:""
+      },
+      option3:{
+        answer:"esta es la respuesta 3",
+        checked:""
+      },
+      option4:{
+        answer:"esta es la respuesta 4",
+        checked:""
+      },
     },
-    answer:"esta es la respuesta 1",
-    score:0,
-    status:""
+    answerCorrect:"esta es la respuesta 1",
+    answer:"",
+    score:0
   },
   {
     id:2,
     question:"esta es una pregunta para la quiz?",
     options:{
-      option1:"esta es la respuesta 1?",
-      option2:"esta es la respuesta 2?",
-      option3:"esta es la respuesta 3?",
-      option4:"esta es la respuesta 4?",
+      option1:{
+        answer:"esta es la respuesta 1",
+        checked:""
+      },
+      option2:{
+        answer:"esta es la respuesta 2",
+        checked:""
+      },
+      option3:{
+        answer:"esta es la respuesta 3",
+        checked:""
+      },
     },
-    answer:"esta es la respuesta 1",
-    score:0,
-    status:""
+    answerCorrect:"esta es la respuesta 1",
+    answer:"",
+    score:0
   }
 ]
 
 const view = (id = 0) =>{
   let answer = document.getElementById("answer")
+  let previus = document.getElementById("previus")
   let question = document.querySelector(".quiz h3")
   let quiz = quizs[id]
   question.innerText = `${quiz.question}`
-  answer.innerHTML = `<input type="hidden" name"id" value="${id}">`
+  answer.innerHTML = `<input type="hidden" name"id-answer" value="${id}">`
   for(const option in quiz.options){
     let label = document.createElement("label")
-    label.innerHTML = `<input type="radio" name="answer" value="${quiz.options[option]}"> ${quiz.options[option]}`
+    label.innerHTML = `<input type="radio" name="answer" value="${quiz.options[option].answer}" ${quiz.options[option].checked}  required> ${quiz.options[option].answer}`
     label.classList = "p-2 m-2"
     answer.append(label)
   }
-}
+  previus.elements[0].value = id
+}  
+    
 view()
 
 const next = () => {
   const next = document.getElementById("next")
   next.addEventListener("submit",(e)=>{
     e.preventDefault()
-    let answer = e.target
-    console.log(answer)
+    let answer = e.target["answer"].value
+    let id = e.target[0].value
+    quizs[id].answerCorrect == answer ? quizs[id].score = 1: false
+    for(const option in quizs[id].options){
+      console.log(typeof option)
+      answer == quizs[id].options[option].answer ? quizs[id].options[option].checked= "checked": false
+    }
+    id++
+    view(id)
+    console.log(quizs)
   })
 }
+
+const previus = () => {
+  const previus = document.getElementById("previus")
+  previus.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    let id = e.target[0].value
+    view(id)
+
+  })
+}
+
+
 next()
+previus()
